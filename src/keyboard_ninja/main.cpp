7424354cc83e825ae22e8418e21f87c6e8115502
KenGuru852 <pritula.alex91@mail.ru>
2022-05-14 16:33:50 +0700

    Added InfoOutput()

--- src/keyboard_ninja/main.cpp
+++ src/keyboard_ninja/main.cpp
@@ -12,11 +12,33 @@ using namespace std;
 string RandomWord(vector<string> dict, int DictSize) {
   return dict[rand() % DictSize];
 }
+
+void InfoOutput(int s, int w, int time){
+	cout << "Right: " << s << endl;
+	cout << "Wrong: " << w << endl;
+	cout << "Time left: " << time << endl;
+}
+
 void Playing(vector<string> dict, int PlayTime, int DictSize) {
+  int score = 0;
+  int wrong = 0;
+  time_t StartTime = time(NULL);
   while (time(NULL) - StartTime < PlayTime) {
     string NewWord = RandomWord(dict, DictSize);
+    InfoOutput(score, wrong, PlayTime - (time(NULL) - StartTime));
     cout << "\n\n\n\t" << NewWord << "\n\n\n\n";
-    sleep(1);
+    string InputWord;
+    bool Equal = false;
+    while (!Equal and(time(NULL) - StartTime < PlayTime)) {
+      cin >> InputWord;
+      if (InputWord == NewWord) {
+        score++;
+        Equal = true;
+      } else {
+        cout << "Try again\n";
+        wrong++;
+      }
+    }
   }
 }
 int main() {
