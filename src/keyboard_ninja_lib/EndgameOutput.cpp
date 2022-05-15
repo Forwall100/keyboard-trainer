eca98c880b4d4428dde9a5cd782fe71984230d20
Forwall100 <hezeri546@gmail.com>
2022-05-15 10:00:39 -0400

    Fixed a bug with the number of words per minute

--- src/keyboard_ninja_lib/EndgameOutput.cpp
+++ src/keyboard_ninja_lib/EndgameOutput.cpp
@@ -10,7 +10,7 @@ void EndGameOutput(int s, int w, int time) {
   cout << "Правильно введенные слова: " << s << endl;
   cout << "НЕправильно введенные слова: " << w << endl;
   cout << "Время тренировки: " << (time < 0 ? 0 : time) << endl;
-  cout << "Кол-во слов в минуту: " << (float)s / 60 << endl;
+  cout << "Кол-во слов в минуту: " << (float)s / ((float)time / 60.0) << endl;
   cout << endl << endl << endl;
   cout << "Сохранить статистику? (y/n)" << endl;
 }
