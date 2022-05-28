0b59fbcbf7c92614d8bf73d7d54ef370d8874e4b
KenGuru852 <pritula.alex91@mail.ru>
2022-05-28 14:55:07 +0700

    Fixed save_statistics

--- src/keyboard_ninja_lib/Save_statistics.cpp
+++ src/keyboard_ninja_lib/Save_statistics.cpp
@@ -6,11 +6,18 @@
 using namespace std;
 
 float Save_statistic(int score, int wrong, int training_time) {
-
+  time_t now = time(NULL);
+  tm *ltm = localtime(&now);
+  auto year = 1900 + ltm->tm_year;
+  auto month = ltm->tm_mon;
+  auto day = 1 + ltm->tm_mday;
+  auto hour = ltm->tm_hour;
+  auto min = ltm->tm_min;
   ofstream out("./statistic.txt", ios::app);
   if (out.is_open()) {
     out << "\n\n";
-    out << "Тренировка " << endl;
+    out << "Тренировка " << day << "." << month << "." << year << " " << hour
+        << ":" << min << endl;
     out << "Правильно введенные слова: " << score << endl;
     out << "НЕправильно введенные слова: " << wrong << endl;
     out << "Время тренировки: " << (training_time < 0 ? 0 : training_time)
