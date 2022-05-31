f9ee50a39df526b22281aa77dfcaa72c4794e50b
Forwall100 <hezeri546@gmail.com>
2022-05-31 14:22:57 +0000

    Fix time output

--- src/keyboard_ninja_lib/InfoOutput.cpp
+++ src/keyboard_ninja_lib/InfoOutput.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int InfoOutput(int s, int w, int time) {
   cout << "Правильно введенные слова: " << s << endl;
   cout << "НЕправильно введенные слова: " << w << endl;
-  cout << "Осталось времени: " << (time < 0 ? 0 : time) << endl;
+  cout << "Осталось времени: " << (time < 0 ? 0 : time) << " сек" << endl;
   return (time < 0 ? 0 : time);
 }
