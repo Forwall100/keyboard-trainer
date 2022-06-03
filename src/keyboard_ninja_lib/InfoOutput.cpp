15f417efbef3b75e4ea1365bfa8ce901e9256038
shebandr <shebanoff.andrei@yandex.ru>
2022-06-03 21:22:21 +0700

    variable name fixes and commentaries in InfoOutput

--- src/keyboard_ninja_lib/InfoOutput.cpp
+++ src/keyboard_ninja_lib/InfoOutput.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 using namespace std;
-
-int InfoOutput(int s, int w, int time) {
-  cout << "Правильно введенные слова: " << s << endl;
-  cout << "НЕправильно введенные слова: " << w << endl;
-  cout << "Осталось времени: " << (time < 0 ? 0 : time) << " сек" << endl;
+// функция получает не вход количество правильных слов, неправильных слов и количество секунд до конца игры
+int InfoOutput(int CorrectWord, int WrongWord, int time) {
+  cout << "Правильно введенные слова: " << CorrectWord << endl;
+  cout << "НЕправильно введенные слова: " << WrongWord << endl;
+  cout << "Осталось времени: " << (time < 0 ? 0 : time) << " сек" << endl; // если время становится отрицательным - выводится 0
   return (time < 0 ? 0 : time);
 }
