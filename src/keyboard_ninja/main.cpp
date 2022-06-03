389c71441efccd234dd00fd9d80d6fd8341f2c77
Forwall100 <hezeri546@gmail.com>
2022-06-03 13:34:57 +0000

    Added launch flag to specify dictionary path

--- src/keyboard_ninja/main.cpp
+++ src/keyboard_ninja/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <keyboard_ninja_lib/EndgameOutput.hpp>
 #include <keyboard_ninja_lib/InfoOutput.hpp>
+#include <keyboard_ninja_lib/ParsingDict.hpp>
 #include <keyboard_ninja_lib/Playing.hpp>
 #include <keyboard_ninja_lib/RandomWord.hpp>
-#include <keyboard_ninja_lib/ParsingDict.hpp>
 #include <random>
 #include <stdlib.h>
 #include <string>
@@ -13,18 +13,27 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
   srand(time(NULL));
 
+  // Если были переданы параметры запуска, то записываем содержимое этого
+  // параметра в DictPath, значение по умолчанию './dict.txt'
+  string DictPath;
+  if (argc > 1) {
+    DictPath += argv[1];
+  } else {
+    DictPath = "./dict.txt";
+  }
+
   vector<string> dict;
-  dict = parsing_dict();
+  dict = parsing_dict(DictPath);
 
   int PlayTime;
   int menu;
   int DictionarySize = dict.size();
 
   if (DictionarySize == 0) {
-    cout << "Файл dict.txt пуст" << endl;
+    cout << "Файл словаря пуст" << endl;
     return 0;
   }
 
