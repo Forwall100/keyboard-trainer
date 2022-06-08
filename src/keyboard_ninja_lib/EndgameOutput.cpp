345322bd73f522b67f7b57a39b61ad934d996a33
shebandr <shebanoff.andrei@yandex.ru>
2022-06-08 14:04:07 +0700

    fixed codestyle EndGameOutput.cpp and Playing.cpp

--- src/keyboard_ninja_lib/EndgameOutput.cpp
+++ src/keyboard_ninja_lib/EndgameOutput.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 // функция получает на вход количество правильных слов, количество неправильных
 // слов и количество секунд, сколько длилась тренировка
-string EndGameOutput(int CorrectWord, int WrongWord, int time)
-{
+string EndGameOutput(int CorrectWord, int WrongWord, int time) {
   // в переменной string_data происходит конкатенация строк для дальнейшего их
   // вывода в терминал и возвращения из функции для теста
   string string_data =
--- src/keyboard_ninja_lib/Playing.cpp
+++ src/keyboard_ninja_lib/Playing.cpp
@@ -1,16 +1,14 @@
+#include <iostream>
 #include <keyboard_ninja_lib/EndgameOutput.hpp>
 #include <keyboard_ninja_lib/InfoOutput.hpp>
 #include <keyboard_ninja_lib/Playing.hpp>
 #include <keyboard_ninja_lib/RandomWord.hpp>
 #include <keyboard_ninja_lib/SaveStatistic.hpp>
-#include <iostream>
 
 using namespace std;
 
-int Playing(vector<string> dict, int PlayTime, int DictSize)
-{
-  if (PlayTime <= 0)
-  {
+int Playing(vector<string> dict, int PlayTime, int DictSize) {
+  if (PlayTime <= 0) {
     return 0;
   } //прекращение работы программы, если введено отрицательное время
 
@@ -19,8 +17,7 @@ int Playing(vector<string> dict, int PlayTime, int DictSize)
   time_t StartTime = time(NULL);
 
   while (time(NULL) - StartTime < PlayTime and
-         ExitFlag == 0)
-  { //пока разность текущего времени и времени старта игры
+         ExitFlag == 0) { //пока разность текущего времени и времени старта игры
     //не будет больше, чем время, выбранное игроком для
     //игры, программа будет работать
     system("clear");
@@ -37,24 +34,19 @@ int Playing(vector<string> dict, int PlayTime, int DictSize)
         false; //флаг для того, чтобы в случае неверного ввода слова, игрок
                //вводил то же самое слово, а не выбиралось случайное
     while (!Equal and (time(NULL) - StartTime < PlayTime) and
-           ExitFlag == 0)
-    {                   //пока игрок не ввёл правильное слово и пока время
-                        //не завершится и пока игрок не завершит игру
-      cin >> InputWord; //ввод слова игроком
-      if (InputWord == ":q")
-      {
+           ExitFlag == 0) { //пока игрок не ввёл правильное слово и пока время
+                            //не завершится и пока игрок не завершит игру
+      cin >> InputWord;     //ввод слова игроком
+      if (InputWord == ":q") {
         wrong--;
         ExitFlag = 1;
       } //для досрочного завершения игры (в случае, если игрок введёт слово
         //":q", игра досрочно завершится
-      if (InputWord == NewWord)
-      {
+      if (InputWord == NewWord) {
         score++;
         Equal = true; //если слово введено верно, игрок получает +1 к числу
                       //правильных слов и выбирается новое слово
-      }
-      else
-      {
+      } else {
         system("clear");
         wrong++;
         InfoOutput(score, wrong, PlayTime - (time(NULL) - StartTime));
@@ -67,8 +59,7 @@ int Playing(vector<string> dict, int PlayTime, int DictSize)
   char choice; //для решения о сохранении статистики
   EndGameOutput(score, wrong, PlayTime);
   cin >> choice;
-  if (choice == 'y')
-  {
+  if (choice == 'y') {
     SaveStatistic(score, wrong, PlayTime);
   }
   system("clear");
